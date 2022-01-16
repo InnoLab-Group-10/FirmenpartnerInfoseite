6b53deb9e86a50852d436a6f21f50bbf0c716808
StephanWeinlinger <stephan.weinlinger@gmail.com>
2022-01-17 00:24:29 +0100

    Edit formatting

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const BottomBar = () => {
-	return <div className="bottom-bar">
-        <Container>
-            <Row>
-                <Col>
-                    © 2020 Company Name. Made With Love By Gruppe 10
-                </Col>
-            </Row>
-        </Container>
-    </div>;
+	return (
+		<div className='bottom-bar'>
+			<Container>
+				<Row>
+					<Col>© 2020 Company Name. Made With Love By Gruppe 10</Col>
+				</Row>
+			</Container>
+		</div>
+	);
 };
 
 export default BottomBar;
